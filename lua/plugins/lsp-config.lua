f4283eb473b575030605a4004722ef1c9cc6bcad
Jonathan4768 <david.abad@jala.university>
2024-09-08 19:31:30 -0600

    add C# lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,24 +1,31 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "jdtls" },
+				automatic_installation = true,
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-jdtls",
+		dependencies = {
+			"mfussenegger/nvim-dap",
 		},
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = false,
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local servers = {
 				"tsserver",
 				"cssls",
@@ -27,14 +34,14 @@ return {
 				"jdtls",
 				"tailwindcss",
 				"pyright",
+				"prettierd",
 				"eslint",
 				"quick_lint_js",
-				"sqlls",
 				"emmet_ls",
+				"csharp_ls",
+				"csharpier",
 				"prismals",
-				"prettierd",
 				"cucumber_language_server",
-				"clangd",
 			}
 			for _, server in ipairs(servers) do
 				lspconfig[server].setup({
@@ -42,10 +49,24 @@ return {
 				})
 			end
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+			vim.keymap.set(
+				"n",
+				"<leader>cr",
+				require("telescope.builtin").lsp_references,
+				{ desc = "[C]ode Goto [R]eferences" }
+			)
+			-- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
+			vim.keymap.set(
+				"n",
+				"<leader>ci",
+				require("telescope.builtin").lsp_implementations,
+				{ desc = "[C]ode Goto [I]mplementations" }
+			)
+			vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
 		end,
 	},
 }
